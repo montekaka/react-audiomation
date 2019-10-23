768537d822294b10d531178f0417e58488da8263
Josh Chen <josh.chen.jia@gmail.com>
2019-10-23 16:42:00 -0700

    changes use web audio context for visual

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import P5Component from './Components/P5Component.jsx'
+// import P5Component from './Components/P5Component.jsx'
+import WebAudioComponent from './Components/WebAudioComponent.jsx'
 // import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">      
-      <P5Component />
+      <WebAudioComponent />
     </div>
   );
 }
--- src/libs/q5.js
+++ src/libs/q5.js
@@ -1,30 +1,51 @@
 import axios from 'axios';
-import p5 from "p5";
-import "p5/lib/addons/p5.sound";
 
-class q5 {
-  constructor(url) {
-    this.url = url;
-    // this.song = {};
-  }
+const calcBarHeight = (barHeight, height) => {
+  const barHeightPercent = barHeight / 255;
+  return Math.floor(barHeightPercent * height);
+};
 
-  init() {
-    const song = new p5.SoundFile(this.url, () => {
-      console.log('loaded')
-      this.song = song;
-      song.play();
-      this.amp = new p5.Amplitude();
-    }, () => {
+const q5 = {}
 
-    }, (a) => {
-      // TODO: show loading percentage
-      console.log(a)
-    });    
-  }
+q5.loadSound = (audioCtx, url) => {
+  return axios({url: url, responseType: "arraybuffer"})
+  .then((res) => {
+    const audioData = res.data;
+    return audioCtx.decodeAudioData(audioData)
+  })
+}
 
-  draw() {
-    
-  }
+q5.play = (source, analyser, dataArray, fromTime, drawcb) => {
+  analyser.minDecibels = -90;
+  analyser.maxDecibels  = -10;
+  analyser.smoothingTimeConstant = 0.85;
+  analyser.fftSize = 256;
+  dataArray.current = new Uint8Array(analyser.frequencyBinCount);
+  source.start(fromTime);
+
+  setInterval(() => {
+    requestAnimationFrame(drawcb);
+  }, 1000/40)
+}
+
+q5.drawWaveForm = (analyser, dataArray, canvasCtx, width, height) => {
+  const bufferLength = analyser.frequencyBinCount;
+
+  analyser.getByteFrequencyData(dataArray);
+  canvasCtx.clearRect(0, 0, width, height);
+  
+  const barWidth = (width - bufferLength) / bufferLength;
+
+  let barHeight;
+  let x = 0;
+  for (let i = 0; i < bufferLength; i++) {
+    barHeight = calcBarHeight(dataArray[i], height);
+    //console.log(_height - barHeight)
+    canvasCtx.fillStyle = "rgb(255, 87, 51)";
+    canvasCtx.fillRect(x, height-barHeight, barWidth, barHeight);
+
+    x += barWidth + 1;
+  }  
 }
 
 export default q5;
